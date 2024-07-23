74cce2e728c14783dffd22c9973ced56ca99371b
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-07-23 15:45:06 +0530

    Time: 5 ms (92.24%) | Memory: 44.6 MB (25.80%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,12 +1,27 @@
 class Solution {
-    public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        // count frequency of each number
-        Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1));
-        // custom sort
-        return Arrays.stream(nums).boxed()
-                .sorted((a, b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
-                .mapToInt(n -> n)
-                .toArray();
+    public int[] frequencySort(int[] nums){
+    Map<Integer,TreeSet<Integer>>map=new TreeMap<>();
+        int[] a=new int[201];
+        for(int i:nums){
+            a[i+100]++;
+        }
+         for(int i:nums){
+            TreeSet<Integer> li=map.getOrDefault(a[100+i],new TreeSet<Integer>());
+            li.add(i);
+            map.put(a[100+i],li);
+            
+        }
+        int j=0;
+        for(int i:map.keySet()){
+            TreeSet<Integer> h=map.get(i);
+            while(!h.isEmpty()){
+                int k=0;
+                int e=h.pollLast();
+                 while(k++<i){
+                nums[j++]=e;
+            }
+            }
+        }
+        return nums;
     }
 }
