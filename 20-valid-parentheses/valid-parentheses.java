370ee194345516f460c2d45be9dfd2993e1b1e49
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-12-15 21:35:48 +0530

    Time: 2 ms (97.51%) | Memory: 42 MB (15.52%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,17 @@
 class Solution {
-    public boolean isValid(String s) {
-        ArrayDeque<Character> stk = new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
-            char x = s.charAt(i);
-            if(x=='(' || x=='{' || x=='[')
-            stk.push(x);
-            else{
-                if(stk.isEmpty()==true)
-                return false;
-                else if(isMatching(stk.peek(),x) == false)
-                return false;
-                else
-                stk.pop();
-            }
-        } 
-           return (stk.isEmpty()==true);
-    }
-    public boolean isMatching(char a,char b){
-        return ((a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']'));
-    }
+  public boolean isValid(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+
+    return stack.isEmpty();
+  }
 }
