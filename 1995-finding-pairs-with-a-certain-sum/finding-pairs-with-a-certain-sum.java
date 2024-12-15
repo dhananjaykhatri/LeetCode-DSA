c71b7d848d10750e1a9a4f0203d67eba5c80eee7
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-12-15 17:09:16 +0530

    Time: 156 ms (39.12%) | Memory: 71.5 MB (94.88%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
@@ -1,36 +1,44 @@
 class FindSumPairs {
-    int arr1[];
-    int arr2[];
-    HashMap<Integer,Integer> hp;
-
+    int[] nums1,nums2;
+    HashMap<Integer,Integer> hm=new HashMap<>();
+    //intialize arrays and hashmap
     public FindSumPairs(int[] nums1, int[] nums2) {
-        arr1 = nums1;
-        arr2 = nums2;
-        hp = new HashMap<>();
-
-        for(int a : arr2){
-            hp.put(a,hp.getOrDefault(a,0)+1);
-        }
+        this.nums1=nums1;
+        this.nums2=nums2;
+        //add nums2 ele to hashmap
+        for(int i:nums2)
+        hm.put(i,hm.getOrDefault(i,0)+1);
     }
-    
     public void add(int index, int val) {
-        hp.put(arr2[index],hp.getOrDefault(arr2[index],0)-1);
-        arr2[index]+=val;
-        hp.put(arr2[index],hp.getOrDefault(arr2[index],0)+1);
+        //get the value of that index and add value+val to map and update hashmap and nums2[index]
+        int v=nums2[index];
+        if(hm.get(v)==1)
+            hm.remove(v); 
+        else
+            hm.put(v,hm.get(v)-1);
+
+        hm.put(v+val,hm.getOrDefault(v+val,0)+1);
+        nums2[index]+=val;
     }
     
     public int count(int tot) {
-        int ans = 0;
-
-        for(int a : arr1){
-            if(hp.containsKey(tot-a)){
-                ans+=hp.get(tot-a);
-            }
+        int cnt=0;
+        for(int i:nums1)
+        {
+            //add dup pairs also
+            if(hm.containsKey(tot-i))
+            cnt+=hm.get(tot-i);
         }
-        return ans;
+        return cnt;
     }
 }
 
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
 /**
  * Your FindSumPairs object will be instantiated and called as such:
  * FindSumPairs obj = new FindSumPairs(nums1, nums2);
