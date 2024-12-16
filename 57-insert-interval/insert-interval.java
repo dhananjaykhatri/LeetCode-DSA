fa0f56083000ebc0ac1c4074715c2cf1e8397543
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-12-16 20:56:19 +0530

    Time: 1 ms (99.09%) | Memory: 45 MB (24.89%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,45 +1,26 @@
-class Solution {
-    public int[][] insert(int[][] inter, int[] newI) {
-        int till = -1, from = -1;
-        int n = inter.length;
-        int[][] res;
-        if(n == 0){
-            res = new int[1][2];
-            res[0] = newI;
-            return res;
+public class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        
+        // add all intervals before newInterval
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i++]);
         }
-        if(newI[1] < inter[0][0]){
-            res = new int[n + 1][2];
-            System.arraycopy(inter, 0, res, 1, n);
-            res[0] = newI;
-            return res;
+        
+        // merge overlapping intervals
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        for(int i = 0; i < n; i++){
-            if(till == -1 && inter[i][1] >= newI[0])
-                till = i;
-            if(newI[1] < inter[i][0]){
-                from = i - 1;
-                break;
-            }
-            if(newI[1] == inter[i][0]){
-                from = i;
-                break;
-            }
+        result.add(newInterval);
+        
+        // add remaining intervals
+        while (i < intervals.length) {
+            result.add(intervals[i++]);
         }
-        if(till == -1){
-            res = new int[n + 1][2];
-            System.arraycopy(inter, 0, res, 0, n);
-            res[n] = newI;
-            return res;
-        }
-        from = from == -1 ? n - 1 : from;
-        newI[0] = Math.min(newI[0], inter[till][0]);
-        newI[1] = Math.max(newI[1], inter[from][1]);
-
-        res = new int[till + n - from][2];
-        res[till] = newI;
-        System.arraycopy(inter, 0, res, 0, till);
-        System.arraycopy(inter, from + 1, res, till+1, n - from - 1);
-        return res;
+        
+        return result.toArray(new int[result.size()][]);
     }
 }
