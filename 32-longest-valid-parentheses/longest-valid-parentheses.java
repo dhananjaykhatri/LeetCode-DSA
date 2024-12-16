dba76aa87ebd02f4f310b0f76484cc7bad48ebb2
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-12-16 21:24:49 +0530

    Time: 1 ms (99.46%) | Memory: 42.3 MB (84.86%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLen = 0;
+        int left = 0;
+        int right = 0;
+
+        // Left to right pass
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                maxLen = Math.max(maxLen, 2 * right);
+            } else if (right > left) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        left = 0;
+        right = 0;
+
+        // Right to left pass
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')') {
+                right++;
+            } else {
+                left++;
+            }
+
+            if (left == right) {
+                maxLen = Math.max(maxLen, 2 * left);
+            } else if (left > right) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        return maxLen;
+    }
+}
