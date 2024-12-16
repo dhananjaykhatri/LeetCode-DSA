b262a0978e4501e327a87d93611cb103fd32d03f
Dhananjay Khatri <149976227+dhananjaykhatri@users.noreply.github.com>
2024-12-16 20:54:25 +0530

    Time: 8 ms (87.77%) | Memory: 46.6 MB (36.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-       Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        Stack <int[]> st= new Stack <>();
-        st.add(intervals[0]);
-        for(int i=0;i<intervals.length;i++)
-        {
-            if(intervals[i][0]<=st.peek()[1])
-            {
-                st.peek()[1]= Math.max(st.peek()[1],intervals[i][1]);
-            }
-            else
-            {
-                st.add(intervals[i]);
+        if (intervals.length == 0) {
+            return ans.toArray(new int[0][]);
+        }
+
+        int[] temp = intervals[0];
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] <= temp[1]) {
+                temp[1] = Math.max(temp[1], intervals[i][1]);
+            } else {
+                ans.add(temp);
+                temp = intervals[i];
             }
         }
-        int ans[][]= new int[st.size()][2];
-        int i= st.size()-1;
-        while(!st.empty())
-        {
-            ans[i][0]=st.peek()[0];
-            ans[i--][1]=st.peek()[1];
-            st.pop();
+        ans.add(temp);
 
-        }
-return(ans);
-        
+        return ans.toArray(new int[0][]);
     }
 }
